18e304f6a9d32c943b4898bcb772456e330148f0
Tom Barber <tom@analytical-labs.com>
2011-10-13 20:19:26 +0100

    changes for artifactory

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,32 +4,24 @@ include tomcat::source
 include apache
 include java
 $tomcat_version = "6.0.18"
-include git::client
-include subversion
 apache::module {"proxy_ajp":
   ensure  => present,
 }
 
-apache::vhost {"ci.analytical-labs.com":
+apache::vhost {"repo.analytical-labs.com":
   ensure => present,
 }
 
-tomcat::instance {"jenkins":
+tomcat::instance {"repo":
   ensure      => present,
   ajp_port    => "8009",
 }
 
-apache::proxypass {"jenkins":
+apache::proxypass {"repo":
   ensure   => present,
   location => "/",
-  vhost    => "ci.analytical-labs.com",
+  vhost    => "repo.analytical-labs.com",
   url      => "ajp://localhost:8009/",
-}
-
-file { "/var/www/ci.analytical-labs.com/htdocs/index.html":
-	ensure => present,
-	source => "puppet:///modules/jenkins/index.html",
-	require => Apache::Vhost["ci.analytical-labs.com"],
 }
 
 file { "/home/tomcat":
