890efb2c5190b30f00e4228311874e94a846b840
Tom Barber <tom@analytical-labs.com>
2011-10-14 11:59:23 +0100

    changes to extdata

---
+++ manifests/definitions/artifactoryinstance.pp
@@ -0,0 +1,33 @@
+define artifactory::instance($ensure, $artifactory_url = 'repo.analytical-labs.com', $artifactory_ajp, $artifactory_http, $artifactory_server) {
+
+include tomcat::source
+include apache
+include java
+$tomcat_version = "6.0.18"
+include apache::mod_proxy
+
+apache::vhost {"${artifactory_url}":
+  ensure => present,
+}
+
+tomcat::instance {"${name}":
+  ensure      => $ensure,
+  ajp_port    => "${artifactory_ajp}",
+  http_port	  => "${artifactory_http}",
+  server_port => "${artifactory_server}",
+}
+
+apache::proxypass {"${name}":
+  ensure   => $ensure,
+  location => "/",
+  vhost    => "${artifactory_url}",
+  url      => "ajp://localhost:${artifactory_ajp}/",
+}
+
+file { "/home/tomcat":
+	ensure => directory,
+	mode => 700,
+	owner => tomcat,
+}
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,32 +1,5 @@
+import "definitions/*.pp"
+
 class artifactory {
 
-include tomcat::source
-include apache
-include java
-$tomcat_version = "6.0.18"
-include apache::mod_proxy
-
-apache::vhost {"repo.analytical-labs.com":
-  ensure => present,
-}
-
-tomcat::instance {"repo":
-  ensure      => present,
-  ajp_port    => "8010",
-  http_port	  => "8081",
-  server_port => "8006",
-}
-
-apache::proxypass {"repo":
-  ensure   => present,
-  location => "/",
-  vhost    => "repo.analytical-labs.com",
-  url      => "ajp://localhost:8010/",
-}
-
-file { "/home/tomcat":
-	ensure => directory,
-	mode => 700,
-	owner => tomcat,
-}
 }
