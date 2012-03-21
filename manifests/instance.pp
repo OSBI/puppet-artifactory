04aaae4c83309b297fc90d03ece9b397155322a8
Tom Barber <tom@analytical-labs.com>
2012-03-21 23:18:54 +0000

    files

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -21,36 +21,38 @@
 #			artifactory_http => $artifactory_http,
 #			artifactory_server => $artifactory_server,
 #	}
-define artifactory::instance($ensure, $artifactory_url = 'repo.analytical-labs.com', $artifactory_ajp, $artifactory_http, $artifactory_server) {
-
-  include tomcat::source
-  include apache
-  include java
-  $tomcat_version = "6.0.18"
-  include apache::mod_proxy
-
-  apache::vhost {"${artifactory_url}":
-    ensure => present,
-  }
-
-  tomcat::instance {"${name}":
-    ensure      => $ensure,
-    ajp_port    => "${artifactory_ajp}",
-    http_port	  => "${artifactory_http}",
-    server_port => "${artifactory_server}",
-  }
-
-  apache::proxypass {"${name}":
-    ensure   => $ensure,
-    location => "/",
-    vhost    => "${artifactory_url}",
-    url      => "ajp://localhost:${artifactory_ajp}/",
-  }
-
-  file { "/home/tomcat":
-    ensure => directory,
-    mode => 700,
-    owner => tomcat,
-  }
-
+define artifactory::instance ($ensure,
+	$artifactory_url = 'repo.analytical-labs.com',
+	$artifactory_ajp,
+	$artifactory_http,
+	$artifactory_server) {
+	include tomcat::source 
+	include apache 
+	include java 
+	
+	$tomcat_version = "6.0.18"
+	include apache::mod_proxy apache::vhost {
+		"${artifactory_url}" :
+			ensure => present,
+	}
+	tomcat::instance {
+		"${name}" :
+			ensure => $ensure,
+			ajp_port => "${artifactory_ajp}",
+			http_port => "${artifactory_http}",
+			server_port => "${artifactory_server}",
+	}
+	apache::proxypass {
+		"${name}" :
+			ensure => $ensure,
+			location => "/",
+			vhost => "${artifactory_url}",
+			url => "ajp://localhost:${artifactory_ajp}/",
+	}
+	file {
+		"/home/tomcat" :
+			ensure => directory,
+			mode => 700,
+			owner => tomcat,
+	}
 }
